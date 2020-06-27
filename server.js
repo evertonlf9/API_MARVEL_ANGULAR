92f4160c9ad53e95196b8b82325d5646b4f54fb2
unknown <everton9dev@gmail.com>
2020-06-27 15:15:46 -0300

    Ajuste para publicar no heroku

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/Front-Star-Wars-Angular/'));
+app.use(express.static('./dist/api-marvel-angular/'));
 
 app.get('/*', (req, res) =>
-    res.sendFile(path.join(__dirname + '/dist/Front-Star-Wars-Angular/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/api-marvel-angular/index.html'))
 );
 
 app.listen(process.env.PORT || 8080);
